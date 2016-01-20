5d69bc2cb30cd3df0c280e46bb9bd94ddb92bab4
naotake <naotawool@users.noreply.github.com>
2016-01-21 00:55:56 +0900

    アラーム情報の更新忘れ対応
    アラーム更新時に更新があるのに、一覧画面へ戻ろうとした場合にダイアログを表示する。

--- app/src/test/java/com/team7/wakeuptaroapp/models/AlarmTest.java
+++ app/src/test/java/com/team7/wakeuptaroapp/models/AlarmTest.java
@@ -1,9 +1,14 @@
 package com.team7.wakeuptaroapp.models;
 
+import com.team7.wakeuptaroapp.exceptions.AlarmConstraintViolationsException;
+
 import org.joda.time.LocalDateTime;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.internal.util.reflection.Whitebox;
+
+import java.util.Arrays;
+import java.util.HashSet;
 
 import static com.team7.wakeuptaroapp.assertions.AlarmAssertion.assertThat;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -60,5 +65,32 @@ public class AlarmTest {
     public void アラーム時刻の情報を取得できること() {
         testee.setTime("19:32");
         assertThat(testee).hasTimeHour(19).hasTimeMinute(32);
+    }
+
+    @Test(expected = AlarmConstraintViolationsException.class)
+    public void 曜日が未選択の場合に例外が送出されること() throws Exception {
+        testee.validate();
+    }
+
+    @Test
+    public void 曜日が1件でも選択されていた場合は例外が送出されないこと() throws Exception {
+        testee.setDayOfWeeks(new HashSet<String>(Arrays.asList("日")));
+        testee.validate();
+        // 何もおきないこと
+    }
+
+    @Test
+    public void 現在値から値の変更があった場合に_true_が返されること() {
+        // 変更無し
+        assertThat(testee.isChanged("", new HashSet<String>(), "")).isTrue();
+
+        // 時間
+        assertThat(testee.isChanged("12:34", new HashSet<String>(), "")).isTrue();
+
+        // 曜日
+        assertThat(testee.isChanged("", new HashSet<String>(Arrays.asList("月")), "")).isTrue();
+
+        // アラーム音
+        assertThat(testee.isChanged("", new HashSet<String>(), "HogeHoge")).isTrue();
     }
 }
